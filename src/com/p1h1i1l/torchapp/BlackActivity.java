f86d0c82727cec4cb0877f025825d0e013d24b40
Phil <batman@Batmans-MacBook-Pro.local>
2014-05-11 12:42:17 +1200

    Torch App
    1

---
+++ src/com/p1h1i1l/torchapp/BlackActivity.java
@@ -0,0 +1,91 @@
+package com.p1h1i1l.torchapp;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.provider.Settings.SettingNotFoundException;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class BlackActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_black);
+
+//		SharedPreferences sp = PreferenceManager
+//				.getDefaultSharedPreferences(this);
+//		boolean cbValue = sp.getBoolean("CHECKBOX", true);
+//
+//		if (cbValue == true) {
+//			lightsOn();
+//		}
+
+		Button onOffButton = (Button) findViewById(R.id.on_off_button);
+		onOffButton.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+
+				lightsOn();
+
+			}
+		});
+
+
+	}
+
+	private void lightsOn() {
+		float curBrightnessValue = 0;
+		try {
+			curBrightnessValue = android.provider.Settings.System.getInt(
+					getContentResolver(),
+					android.provider.Settings.System.SCREEN_BRIGHTNESS);
+		} catch (SettingNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		Intent intent = new Intent(BlackActivity.this, WhiteActivity.class);
+		startActivity(intent);
+		overridePendingTransition(R.anim.appear, R.anim.appear);
+		// finish();
+
+		setBrightness(curBrightnessValue);
+	}
+
+	private void setBrightness(float brightness) {
+		WindowManager.LayoutParams layoutParams = getWindow().getAttributes();
+		layoutParams.screenBrightness = brightness / 255;
+		getWindow().setAttributes(layoutParams);
+	}
+
+	public void setBrightnessNEW(float brightness) {
+		try {
+			int brightnessMode = android.provider.Settings.System.getInt(
+					getContentResolver(),
+					android.provider.Settings.System.SCREEN_BRIGHTNESS_MODE);
+
+			if (brightnessMode == android.provider.Settings.System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC) {
+				android.provider.Settings.System
+						.putInt(getContentResolver(),
+								android.provider.Settings.System.SCREEN_BRIGHTNESS_MODE,
+								android.provider.Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL);
+			}
+
+			WindowManager.LayoutParams layoutParams = getWindow()
+					.getAttributes();
+			layoutParams.screenBrightness = brightness;
+			getWindow().setAttributes(layoutParams);
+
+		} catch (Exception e) {
+			// do something useful
+		}
+	}
+
+}
